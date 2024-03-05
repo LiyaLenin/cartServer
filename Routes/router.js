a1b08fc8f026f29ff7ced4f0cf5efd89e22fcafa
Liya Lenin <liyaleninliya@gmail.com>
2024-03-05 23:39:12 +0530

    updated server

--- Routes/router.js
+++ Routes/router.js
@@ -1,7 +1,13 @@
 const express=require('express')
 const productController=require('../Controllers/productController')
+const userController=require('../Controllers/userController')
 
 const router=new express.Router()
 //getAllProducts
 router.get('/all-products',productController.getAllProductController)
+//login
+router.post('/login',userController.login)
+//register
+router.post('/register',userController.register)
+
  module.exports=router
