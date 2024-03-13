4eea65462ad1b0f72d94b7e173d02db4b4483bef
Liya Lenin <liyaleninliya@gmail.com>
2024-03-13 16:15:04 +0530

    updated

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -8,3 +8,14 @@ exports.getAllProductController=async(req,res)=>{
         res.status(401).json(err)
     }
 }
+ exports.getAProductController=async (req,res)=>{
+    const {id}=req.params
+    try{
+        const result=await products.findOne({id})
+        res.status(200).json(result)
+
+        
+    }catch(err){
+        res.status(401).json(err)
+    }
+ }
--- Routes/router.js
+++ Routes/router.js
@@ -1,13 +1,42 @@
 const express=require('express')
 const productController=require('../Controllers/productController')
 const userController=require('../Controllers/userController')
+const jwtMiddleware = require('../Middlewares/jwtMiddleware')
+const wishlistController=require('../Controllers/wishlistController')
+const cartContoller=require('../Controllers/cartContoller')
 
 const router=new express.Router()
+
 //getAllProducts
 router.get('/all-products',productController.getAllProductController)
 //login
 router.post('/login',userController.login)
 //register
 router.post('/register',userController.register)
+//view product
+router.get('/view-product/:id',productController.getAProductController)
+//addToWishlist
+router.post('/add-to-wishlist',jwtMiddleware,wishlistController.addToWishlistConroller)
+//get wishlist
+router.get('/get-wishlist',jwtMiddleware,wishlistController.getWishlistController)
+//remove wishlist
+router.delete('/wishlist-remove/:id',jwtMiddleware,wishlistController.removeProductWishlistController)
+//addtocart
+router.post('/add-to-cart',jwtMiddleware,cartContoller.addToCartController)
+//get cart
+router.get('/get-cart',jwtMiddleware,cartContoller.getCartController)
+
+// remove cart item
+router.delete('/remove-cart/:id', jwtMiddleware, cartContoller.removeCartItemController)
+
+// increment cart
+router.get('/cart-increment/:id', jwtMiddleware, cartContoller.incrementItemController)
+
+// decrement cart
+router.get('/cart-decrement/:id', jwtMiddleware, cartContoller.decrementItemController)
+
+// empty cart
+router.delete('/empty-cart', jwtMiddleware, cartContoller.emptyCartController)
+
 
  module.exports=router
